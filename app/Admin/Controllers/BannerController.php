f8557a449f88a560cfe0bdf06a76c2e089e69b50
梅渭甲 <meiweijia@vip.qq.com>
2018-12-09 18:52:51 +0800

    add banner basic code

--- app/Admin/Controllers/BannerController.php
+++ app/Admin/Controllers/BannerController.php
@@ -2,10 +2,127 @@
 
 namespace App\Admin\Controllers;
 
-
+use App\Models\Banner;
 use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
 
 class BannerController extends Controller
 {
+    use HasResourceActions;
 
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('轮播图')
+            ->description('列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('轮播图')
+            ->description('详细')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('轮播图')
+            ->description('编辑')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('轮播图')
+            ->description('创建')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Banner);
+
+        $grid->id('ID');
+        $grid->path('路径');
+        $grid->display('是否显示');
+        $grid->sort('顺序');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Banner::findOrFail($id));
+
+        $show->id('ID');
+        $show->path('路径');
+        $show->display('是否显示');
+        $show->sort('顺序');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Banner);
+
+        $form->image('path', '图片路径')->uniqueName();
+        $form->number('sort', '顺序')->default(1);
+        $form->switch('display', '是否显示')->default(true);
+        return $form;
+    }
 }
---
+++ database/migrations/2018_12_09_083251_create_banners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->increments('id')->autoIncrement();
+            $table->string('path')->comment('图片地址');
+            $table->boolean('display')->comment('是否显示');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
