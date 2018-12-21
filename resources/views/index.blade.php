9bacd3105fd344d17b113e94541e603be7c73416
梅渭甲 <meiweijia@vip.qq.com>
2018-12-21 20:49:20 +0800

    首页底部修改

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <link href="https://cdn.bootcss.com/Swiper/4.4.5/css/swiper.min.css" rel="stylesheet">
     <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://simpleqode.bitbucket.io/touche/assets/css/theme.min.css">
+    <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/index.css">
 
 </head>
@@ -111,16 +112,24 @@
     <div id="fh5co-footer" class="animated" style="display: none;">
         <div class="container">
             <div class="row">
-                {{--<div class="col-md-6 text-center">--}}
-                    {{--<p class="to-animate fadeIn animated">Address:{{ config('address') }}</p>--}}
-                {{--</div>--}}
-                {{--<div class="col-md-6 text-center">--}}
-                    {{--<p class="to-animate fadeIn animated">Address:{{ config('address') }}</p>--}}
-                {{--</div>--}}
-                <div class="col-md-12 text-center">
-                    <p class="to-animate fadeIn animated">© {{date('Y')}} {{ config('company_name') }}</p>
+                <div class="col-md-3"></div>
+                <div class="col-md-3 text-center"><p><i class="fa fa-home"></i> 3rd Floor, Building 6, Wantou Industrial Zone, Hongxing Village, Songgang, Bao'an District, Shenzhen</p></div>
+                <div class="col-md-6 text-center">
+                    <div class="row">
+                        <div class="col-md-12 text-left">
+                            <i class="fa fa-phone"> +86-13510717855</i>
+                        </div>
+                        <div class="col-md-12 text-left">
+                            <i class="fa fa-whatsapp"> +86-13510717855</i>
+                        </div>
+                        <div class="col-md-12 text-left">
+                            <i class="fa fa-envelope-o"> Greenfiretech@163.com</i>
+                        </div>
+                    </div>
                 </div>
-
+                <div class="col-md-12 text-center">
+                    <p>© {{date('Y')}} {{ config('product_name') }}  All rights reserved.</p>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <title>@yield('title') - {{ config('product_name') }}</title>
     <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://simpleqode.bitbucket.io/touche/assets/css/theme.min.css">
+    <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/product.css">
     @yield('css')
 </head>
@@ -17,7 +18,8 @@
         <a class="navbar-brand d-lg-none" href="#">{{ config('product_name') }}</a>
 
         <!-- Navbar: Toggler -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -57,8 +59,24 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-3 text-center"><p><i class="fa fa-home"></i> 3rd Floor, Building 6, Wantou Industrial
+                    Zone, Hongxing Village, Songgang, Bao'an District, Shenzhen</p></div>
+            <div class="col-md-6 text-center">
+                <div class="row">
+                    <div class="col-md-12 text-left">
+                        <p><i class="fa fa-phone"> +86-13510717855</i></p>
+                    </div>
+                    <div class="col-md-12 text-left">
+                        <p><i class="fa fa-whatsapp"> +86-13510717855</i></p>
+                    </div>
+                    <div class="col-md-12 text-left">
+                        <p><i class="fa fa-envelope-o"> Greenfiretech@163.com</i></p>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-12 text-center">
-                <p>© {{date('Y')}} {{ config('company_name') }}</p>
+                <p>© {{date('Y')}} {{ config('product_name') }} All rights reserved.</p>
             </div>
         </div>
     </div>
