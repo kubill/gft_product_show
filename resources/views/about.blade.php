81069ba2bf9059f052cd1751e63968a0a6c833ba
梅渭甲 <meiweijia@vip.qq.com>
2018-12-23 11:04:16 +0800

    QC

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,16 +5,12 @@
 @section('content')
     <h1 class="text-center">ABOUT US</h1>
     <h2>About</h2>
-    <p class="lead">Green fire is the local brand of Shenzhen, the global electronic cigarette production
+    <p class="lead">Green fire is the local brand of Shenzhen, the global e-cigarette production
         base, Is a famous OEM production company own brand.</p>
     <p class="lead">Companies adhere to independent research and development, Create a unique e-cigarette brand.</p>
     <p class="lead">Green fire insist on quality first, Cater to the needs of players, Combine health needs, Developed several mainstream market explosions,
         We are also the first to explore safe batteries. One of the e-cigarette companies with technologies such as closed smoke oil systems, These technologies have become industry standards.</p>
-    <img src="images/1.jpg" class="img-fluid" alt="Responsive image">
-    {{--<blockquote class="blockquote">--}}
-    {{--<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>--}}
-    {{--<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>--}}
-    {{--</blockquote>--}}
+    <img src="images/1.jpg" class="img-fluid">
 @endsection
 
 @section('js')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://simpleqode.bitbucket.io/touche/assets/css/theme.min.css">
     <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/product.css">
 
 </head>
 
@@ -27,40 +27,29 @@
     <div class="swiper-pagination"></div>
 </div>
 
-<div class="content"></div>
-<div class="footer">
-    <div id="fh5co-featured" data-section="features" style="display: none;">
-        <div class="container">
-            <div class="row text-center fh5co-heading row-padded">
-                <div class="col-md-12">
-                    <h2 class="heading to-animate navbar-brand">Quality Control</h2>
-                    {{--<p class="sub-heading to-animate">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>--}}
-                </div>
+<div class="container-index" style="">
+    <div class="container-fluid text-center" style="padding-bottom: 4rem;">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="heading to-animate navbar-brand">Quality Control</h2>
             </div>
-            <div class="row">
-                <div class="fh5co-grid">
-                    @foreach ($products as $product)
-                        <div class="fh5co-v-half to-animate-2">
-                            <div class="fh5co-v-col-2 fh5co-bg-img"
-                                 style="background-image: url({{ asset('uploads/'.$product->image) }})"></div>
-                            <div class="fh5co-v-col-2 fh5co-text fh5co-special-1 arrow-left">
-                                <h2>{{ $product->name }}</h2>
-                                <span class="pricing">￥{{ $product->price }}</span>
-                                <p>{{ $product->describe }}</p>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
+            <div class="col-md-4">
+                <img src="images/qc1.jpg" class="img-fluid">
             </div>
-
+            <div class="col-md-4">
+                <img src="images/qc2.jpg" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <img src="images/qc3.jpg" class="img-fluid">
+            </div>
         </div>
     </div>
-
-    <div id="fh5co-footer" class="animated" style="display: none;">
+    <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-md-3"></div>
-                <div class="col-md-3 text-center"><address><p><i class="fa fa-home"></i> 3rd Floor, Building 6, Wantou Industrial Zone, Hongxing Village, Songgang, Bao'an District, Shenzhen</p></address></div>
+                <div class="col-md-3 text-center"><p><i class="fa fa-home"></i> 3rd Floor, Building 6, Wantou Industrial
+                        Zone, Hongxing Village, Songgang, Bao'an District, Shenzhen</p></div>
                 <div class="col-md-6 text-center">
                     <div class="row">
                         <div class="col-md-12 text-left">
@@ -79,8 +68,7 @@
                 </div>
             </div>
         </div>
-    </div>
-
+    </footer>
 </div>
 
 
