ce61f523fe6b0b6f4f950605556a3c8744f42b7a
梅渭甲 <meiweijia@vip.qq.com>
2018-12-23 10:49:56 +0800

    关于修改

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,12 +5,15 @@
 @section('content')
     <h1 class="text-center">ABOUT US</h1>
     <h2>About</h2>
-    <p class="lead">绿火起源电子烟 绿火起源电子烟是全球电子烟生产基地深圳的本土品牌,是知名OEM生产公司自有品牌。公司坚持自主研发,打造独具特色的电子烟品牌。
-        圣尊坚持品质至上,迎合玩家需求,结合健康需要,开发出数款市场主流爆款。绿火起源也是最早探索安全电池、封闭式烟油系统等技术的电子烟公司之一,这些技术已成为行业标准。</p>
+    <p class="lead">Green fire is the local brand of Shenzhen, the global electronic cigarette production
+        base, Is a famous OEM production company own brand.</p>
+    <p class="lead">Companies adhere to independent research and development, Create a unique e-cigarette brand.</p>
+    <p class="lead">Green fire insist on quality first, Cater to the needs of players, Combine health needs, Developed several mainstream market explosions,
+        We are also the first to explore safe batteries. One of the e-cigarette companies with technologies such as closed smoke oil systems, These technologies have become industry standards.</p>
     <img src="images/1.jpg" class="img-fluid" alt="Responsive image">
     {{--<blockquote class="blockquote">--}}
-        {{--<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>--}}
-        {{--<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>--}}
+    {{--<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>--}}
+    {{--<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>--}}
     {{--</blockquote>--}}
 @endsection
 
