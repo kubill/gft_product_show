fb2fbee3f70ef5cb91008cf3a0543e1781a6bc70
kubill <meiweijia@vip.qq.com>
2019-01-20 12:09:04 +0800

    留言后台

---
+++ app/Admin/Controllers/CommentController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Comment;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class CommentController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('留言管理')
+            ->description('列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('留言管理')
+            ->description('详情')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Comment);
+
+        $grid->id('Id');
+        $grid->name('姓名');
+        $grid->email('Email');
+        $grid->telephone('电话号码');
+        $grid->comment('内容');
+        $grid->created_at('留言时间');
+
+        $grid->disableCreateButton();
+        $grid->actions(function (Grid\Displayers\Actions $actions){
+            $actions->disableEdit();
+        });
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Comment::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('姓名');
+        $show->email('Email');
+        $show->telephone('电话号码');
+        $show->comment('内容');
+        $show->created_at('留言时间');
+
+        $show->panel()
+            ->tools(function (Show\Tools $tools) {
+                $tools->disableEdit();
+            });;
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Comment);
+
+        $form->text('name', 'Name');
+        $form->email('email', 'Email');
+        $form->text('telephone', 'Telephone');
+        $form->textarea('comment', 'Comment');
+        $form->number('child_id', 'Child id');
+
+        return $form;
+    }
+}
